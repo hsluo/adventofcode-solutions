07b99d51ab84113e57e252edd02e34e421db72b3
hsluo <horseluo@gmail.com>
2015-12-06 00:36:09 +0800

    add day 5 part 2

--- 5.go
+++ 5.go
@@ -9,20 +9,23 @@ import (
 
 func main() {
 	f, _ := os.Open("input5")
-	count := 0
+	var count1, count2 int
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		if nice(scanner.Text()) {
-			count += 1
+		if nice1(scanner.Text()) {
+			count1 += 1
+		}
+		if nice2(scanner.Text()) {
+			count2 += 1
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(count)
+	fmt.Println(count1, count2)
 }
 
-func nice(input string) bool {
+func nice1(input string) bool {
 	bad := []string{"ab", "cd", "pq", "xy"}
 	for i := range bad {
 		if strings.Contains(input, bad[i]) {
@@ -45,4 +48,24 @@ func nice(input string) bool {
 		}
 	}
 	return false
+}
+
+func nice2(input string) bool {
+	var prop1, prop2 bool
+
+	for i := 0; i < len(input)-1; i++ {
+		pair := string([]byte{input[i], input[i+1]})
+		if strings.Count(input, pair) > 1 {
+			prop1 = true
+			break
+		}
+	}
+
+	for i := 0; i < len(input)-2; i++ {
+		if input[i] == input[i+2] {
+			prop2 = true
+			break
+		}
+	}
+	return prop1 && prop2
 }
--- 5_test.go
+++ 5_test.go
@@ -2,10 +2,10 @@ package main
 
 import "testing"
 
-func TestNice(t *testing.T) {
+func TestNice1(t *testing.T) {
 	nices := []string{"ugknbfddgicrmopn", "aaa"}
 	for i := range nices {
-		if !nice(nices[i]) {
+		if !nice1(nices[i]) {
 			t.Log(nices[i])
 			t.Fail()
 		}
@@ -13,7 +13,24 @@ func TestNice(t *testing.T) {
 
 	naughties := []string{"jchzalrnumimnmhp", "haegwjzuvuyypxyu", "dvszwmarrgswjxmb"}
 	for i := range naughties {
-		if nice(naughties[i]) {
+		if nice1(naughties[i]) {
+			t.Log(naughties[i])
+			t.Fail()
+		}
+	}
+}
+
+func TestNice2(t *testing.T) {
+	nices := []string{"qjhvhtzxzqqjkmpb", "xxyxx"}
+	for i := range nices {
+		if !nice2(nices[i]) {
+			t.Log(nices[i])
+			t.Fail()
+		}
+	}
+	naughties := []string{"uurcxstgmygtbstg", "ieodomkazucvgmuy"}
+	for i := range naughties {
+		if nice1(naughties[i]) {
 			t.Log(naughties[i])
 			t.Fail()
 		}
